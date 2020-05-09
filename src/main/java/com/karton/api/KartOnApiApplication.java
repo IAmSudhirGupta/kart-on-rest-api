20c73c0327ac469f0afa9ca0429a76300e81eba7
Sudhir Gupta <sudhirhitit@gmail.com>
2020-05-10 03:40:46 +0530

    Initial commit

---
+++ src/main/java/com/karton/api/KartOnApiApplication.java
@@ -0,0 +1,13 @@
+package com.karton.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartOnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartOnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karton/api/KartOnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karton.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartOnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
